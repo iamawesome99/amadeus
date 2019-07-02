7dc314249040c3a5061f84860b342ca65e69f795
iamawesome99 <47467642+iamawesome99@users.noreply.github.com>
2019-07-02 08:28:26 -0400

    attempted fix for imgur but non i.imgur.com links

--- UrlHandler.py
+++ UrlHandler.py
@@ -7,21 +7,21 @@ url = imgur_url.ImgurURL()
 
 def handle(link):
 
-    print(link)
-
     if "i.reddit.com" in link:
         return link
 
     if "i.redd.it" in link:
         return link
 
-    elif "i.imgur.com" in link:
-        return link
-
     elif "imgur.com/a" in link:
         link = url.get_imgur_urls('link')[0]
         return link
-        # TODO: Remove imgur_url dependency by implement own function
+        # TODO: Remove imgur_url dependency by implementing own function
+
+    elif "imgur.com" in link:
+        return link
+        # TODO: check if this works for non i.imgur.com links
+        # like in https://www.reddit.com/r/rule34/comments/c1ycf9/bowsette_darkmoney1_super_mario_bros/
 
     elif "gfycat.com" in link:
         response = urllib.request.urlopen(link)
