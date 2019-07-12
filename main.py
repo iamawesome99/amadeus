a28ae8002b819b8caf14a0344b48b0e87b6f01f5
iamawesome99 <47467642+iamawesome99@users.noreply.github.com>
2019-07-12 16:18:52 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from Client import Client
-import json
-
-with open('config.json') as json_data_file:
-    data = json.load(json_data_file)
-    token = data['discord']['token']
-
-client = Client()
-client.run(token)
